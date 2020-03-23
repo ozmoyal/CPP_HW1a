4113ca26aedf9199b5d672c5f420804e9a59d4f8
shirelIsr <shirel.isr@gmail.com>
2020-03-23 21:18:04 +0200

    test

--- Test.cpp
+++ Test.cpp
@@ -15,8 +15,7 @@ using namespace phonetic;
 using namespace std;
 
 
-
-TEST_CASE("Test replacement of p and b") {
+TEST_CASE("Test replacement of p ,b and f") {
     string text = "xxx happy yyy";
     CHECK(find(text, "happy") == string("happy"));
     CHECK(find(text, "habby") == string("happy"));
@@ -25,6 +24,24 @@ TEST_CASE("Test replacement of p and b") {
     text = "xxx pe yyy";
     CHECK(find(text, "be") == string("pe"));
     CHECK(find(text, "pe") == string("be"));
+    text =  "Blame doesn’t fix bugs." ;
+       CHECK(find(text, "flame") == string("blame"));
+         CHECK(find(text, "Blame") == string("blame"));
+    CHECK(find(text, "pix") == string("fix"));
+     CHECK(find(text, "Bix") == string("fix"));
+     CHECK(find(text, "fIX") == string("fix"));
+        CHECK(find(text, "BUGS") == string("bugs"));
+     CHECK(find(text, "fUGS") == string("bugs"));
+     text ="Don’t just Pix the Bugs; BIX whatever Fermitted the BUGS in the Pirst Flace.";
+         CHECK(find(text, "fix") == string("Pix"));
+         CHECK(find(text, "bugs") == string("Bugs"));
+    CHECK(find(text, "pix") == string("fix"));
+     CHECK(find(text, "BIX") == string("fix"));
+     CHECK(find(text, "fIX") == string("fix"));
+        CHECK(find(text, "BUGS") == string("bugs"));
+     CHECK(find(text, "Fermitted") == string("permitted"));
+      CHECK(find(text, "Flace") == string("place"));
+
     /* Add more checks here */
 }
 
@@ -48,5 +65,62 @@ TEST_CASE("Test replacement of v and w") {
     /* Add more checks here */
 }
 
+TEST_CASE("Test replacement of d and t") {
+    string text = "Don’t fix it if id ain’t broke.";
+    CHECK(find(text, "Don’t") == string("Don’d"));
+    CHECK(find(text, "Don’t") == string("Ton’t"));
+    CHECK(find(text, "Don’t") == string("don't"));
+    CHECK(find(text, "id") == string("it"));
+    CHECK(find(text, "it") == string("id"));
+    CHECK(find(text, "ain’d") == string("ain’t"));
+}
+
+TEST_CASE("Test replacement of i and y") {
+    string text = "Don’t fyx it if yt ayn’t broke.";
+    CHECK(find(text, "fix") == string("fyx"));
+    CHECK(find(text, "yt") == string("it"));
+    CHECK(find(text, "yf") == string("if"));
+    CHECK(find(text, "ayn’t") == string("ain’t"));
+    CHECK(find(text, "IT") == string("it"));
+    CHECK(find(text, "IF") == string("if"));
+}
+
+TEST_CASE("Test replacement of o and u") {
+    string text = "Good programmers write code for humans first and computers next.";
+    CHECK(find(text, "guud") == string("good"));
+    CHECK(find(text, "guod") == string("Good"));
+    CHECK(find(text, "goud") == string("Good"));
+    CHECK(find(text, "prUgrammers") == string("programmers"));
+    CHECK(find(text, "cude") == string("code"));
+    CHECK(find(text, "homans") == string("humans"));
+     CHECK(find(text, "cumputers") == string("computers"));
+}
+
+TEST_CASE("Test replacement of s and z") {
+    string text = "Software testers always go to heaven; they’ve already had their fair share of hell.”";
+    CHECK(find(text, "zoftware") == string("Software"));
+    CHECK(find(text, "tezters") == string("testers"));
+     CHECK(find(text, "testerz") == string("testers"));
+    CHECK(find(text, "alwayz") == string("always"));
+    CHECK(find(text, "zhare") == string("share"));
+ 
+}
+
+TEST_CASE("Test replacement of  g and j") {
+    string text = "Good programmers write code for humans first and computers next.”";
+    CHECK(find(text, "jood") == string("Good"));
+    CHECK(find(text, "projrammers") == string("programmers"));
+  
+ 
+}
+
+
+TEST_CASE("Test emptey text and word") {
+    string text = "xxx vorry yyy";
+    CHECK(find(text, "") == string("vorry"));
+    text = "";
+    CHECK(find(text, "be") == string("pe"));
+    /* Add more checks here */
+}
 
 /* Add more test cases here */
