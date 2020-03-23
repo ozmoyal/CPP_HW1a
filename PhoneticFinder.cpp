484bd92eb68676b529050120d5e34ae26f282fdf
ozmoyal <ozzmoyal@gmail.com>
2020-03-23 18:55:23 +0200

    first

---
+++ PhoneticFinder.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <iostream>
+#include "PhoneticFinder.hpp"
+using namespace std;
+namespace phonetic{
+string phonetic::find(string a , string b){
+    return b;
+}
+
+
+}
---
+++ PhoneticFinder.hpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <string>
+
+namespace phonetic {
+std::string find(std::string a , std::string b);
+};
